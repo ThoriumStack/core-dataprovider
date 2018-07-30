e44b83c48a9f754b6a5c5c25b373fd87e90c69e2
Ruan Strydom <ruans@mybucks.com>
2018-07-30 13:14:35 +0200

    Implement before save logic for base models

--- ContextBase.cs
+++ ContextBase.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MyBucks.Core.Model.DataModel;
 
 namespace MyBucks.Core.DataProvider
 {
@@ -22,6 +23,8 @@ namespace MyBucks.Core.DataProvider
         }
         
         public string ConnectionString { get; set; }
+        public int CurrentUserId { get; set; }
+        public string CurrentContext { get; set; }
 
         public override int SaveChanges()
         {
@@ -30,9 +33,51 @@ namespace MyBucks.Core.DataProvider
                 .ToList();
            
             _stateTracker.TriggerStateTrackers(changes);
+            
+            PreSaveLogic();
+            
             var result = base.SaveChanges();
 
             return result;
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            PreSaveLogic();
+                
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            PreSaveLogic();
+                
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void PreSaveLogic()
+        {
+            var baseContextEntries = this.ChangeTracker.Entries<BaseContextModel>().Where(x => x.State == EntityState.Added).ToArray();
+            foreach (var entry in baseContextEntries)
+            {
+                entry.Entity.Context = CurrentContext;
+            }
+            
+            var baseEntries = this.ChangeTracker.Entries<BaseModel>()
+                .Where(x => x.State != EntityState.Unchanged)
+                .ToArray();
+            
+            foreach (var entry in baseEntries.Where(x => x.State == EntityState.Modified))
+            {
+                entry.Entity.ModifiedById = CurrentUserId;
+                entry.Entity.ModifiedDate = DateTime.Now;
+            }
+            
+            foreach (var entry in baseEntries.Where(x => x.State == EntityState.Added))
+            {
+                entry.Entity.CreatedById = CurrentUserId;
+                entry.Entity.CreatedDate = DateTime.Now;
+            }
         }
     }
 }
